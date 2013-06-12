c3b0d790c1e0eb0b0a79dc1d86ec5cb5df6ea88b
Tobias Schlitt <toby@php.net>
2013-06-12 23:10:48 +0200

    Implemented: ChangeRecorder class to record changes for a revision.

---
+++ src/main/Qafoo/ChangeTrack/ChangeRecorder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Qafoo\ChangeTrack;
+
+class ChangeRecorder
+{
+    private $revision;
+
+    private $commitMessage;
+
+    private $changes = array();
+
+    public function __construct($revision, $commitMessage)
+    {
+        $this->revision = $revision;
+        $this->commitMessage = $commitMessage;
+    }
+
+    public function recordChange(\ReflectionClass $class, \ReflectionMethod $method)
+    {
+        if (!isset($this->changes[$class->getName()])) {
+            $this->changes[$class->getName()] = array();
+        }
+        $this->changes[$class->getName()][$method->getName()] = 1;
+    }
+
+    public function getChanges()
+    {
+        return $this->changes;
+    }
+}
---
+++ test/units/Qafoo/ChangeTrack/ChangeRecorderTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Qafoo\ChangeTrack;
+
+class ChangeRecorderTest extends \PHPUnit_Framework_TestCase
+{
+    public function testRecordChange()
+    {
+        $changeRecorder = new ChangeRecorder('abc', 'Message');
+
+        $reflectionClass = new \ReflectionClass(__CLASS__);
+        $reflectionMethod = $reflectionClass->getMethod(__FUNCTION__);
+
+        $changeRecorder->recordChange($reflectionClass, $reflectionMethod);
+
+        $this->assertEquals(
+            array(
+                __CLASS__ => array(
+                    __FUNCTION__ => 1
+                )
+            ),
+            $changeRecorder->getChanges()
+        );
+    }
+
+    public function testRecordChangeDouble()
+    {
+        $changeRecorder = new ChangeRecorder('abc', 'Message');
+
+        $reflectionClass = new \ReflectionClass(__CLASS__);
+        $reflectionMethod = $reflectionClass->getMethod(__FUNCTION__);
+
+        $changeRecorder->recordChange($reflectionClass, $reflectionMethod);
+        $changeRecorder->recordChange($reflectionClass, $reflectionMethod);
+
+        $this->assertEquals(
+            array(
+                __CLASS__ => array(
+                    __FUNCTION__ => 1
+                )
+            ),
+            $changeRecorder->getChanges()
+        );
+    }
+
+    public function testRecordMultipleChanges()
+    {
+        $changeRecorder = new ChangeRecorder('abc', 'Message');
+
+        $testClass = new \ReflectionClass(__CLASS__);
+        $testMethod = $testClass->getMethod(__FUNCTION__);
+
+        $productionClass = new \ReflectionClass('Qafoo\\ChangeTrack\\ChangeRecorder');
+        $productionMethod = $productionClass->getMethod('recordChange');
+
+        $changeRecorder->recordChange($testClass, $testMethod);
+        $changeRecorder->recordChange($productionClass, $productionMethod);
+
+        $this->assertEquals(
+            array(
+                __CLASS__ => array(
+                    __FUNCTION__ => 1
+                ),
+                'Qafoo\\ChangeTrack\\ChangeRecorder' => array(
+                    'recordChange' => 1
+                )
+            ),
+            $changeRecorder->getChanges()
+        );
+    }
+}
