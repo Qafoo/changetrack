f37abf0e74fdbd66b147dacf26a1606f19d84ae4
Tobias Schlitt <toby@php.net>
2013-06-13 15:38:02 +0200

    Implemented: Line feed generator for diff chunks.

---
+++ src/main/Qafoo/ChangeTrack/Analyzer/ChunkLineFeedGenerator.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Qafoo\ChangeTrack\Analyzer;
+
+use Arbit\VCSWrapper\Diff;
+
+class ChunkLineFeedGenerator
+{
+    private $chunk;
+
+    public function __construct(Diff\Chunk $chunk)
+    {
+        $this->chunk = $chunk;
+    }
+
+    public function feedLines()
+    {
+        $lineCount = count($this->chunk->lines);
+        $lineNumber = $this->chunk->end;
+
+        for ($lineOffset = 0; $lineOffset < $lineCount; $lineOffset++) {
+            $line = $this->chunk->lines[$lineOffset];
+
+            switch ($line->type) {
+                case Diff\Line::ADDED:
+                    yield $lineNumber;
+                    $lineNumber++;
+                    break;
+                case Diff\Line::UNCHANGED:
+                    $lineNumber++;
+                    break;
+                case Diff\Line::REMOVED:
+                    yield $lineNumber;
+                    break;
+            }
+        }
+
+    }
+}
---
+++ test/integration/Qafoo/ChangeTrack/Analyzer/ChunkLineFeedGeneratorTest.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Qafoo\ChangeTrack\Analyzer;
+
+use Arbit\VCSWrapper\Diff;
+
+class ChunkLineFeedGeneratorTest extends \PHPUnit_Framework_TestCase
+{
+    public function testIteratePureAddedDiff()
+    {
+        $parser = new Diff\Unified();
+        $diff = $parser->parseString(file_get_contents(__DIR__ . '/_fixtures/diff_add_only.diff'));
+
+        $lineFeedGenerator = new ChunkLineFeedGenerator($diff[0]->chunks[0]);
+
+        $this->assertLinesFed(
+            array(21, 22),
+            $lineFeedGenerator
+        );
+    }
+
+    public function testIteratePureRemovedDiff()
+    {
+        $diff = $this->loadDiff('diff_remove_only.diff');
+
+        $lineFeedGenerator = new ChunkLineFeedGenerator($diff[0]->chunks[0]);
+
+        $this->assertLinesFed(
+            array(22, 22),
+            $lineFeedGenerator
+        );
+    }
+
+    /**
+     * @test testIteratePureAddedDiff
+     * @test testIteratePureRemovedDiff
+     */
+    public function testIterateAddedAndRemovedDiff()
+    {
+        $diff = $this->loadDiff('diff_add_and_remove.diff');
+
+        $lineFeedGenerator = new ChunkLineFeedGenerator($diff[0]->chunks[0]);
+
+        $this->assertLinesFed(
+            array(22, 22),
+            $lineFeedGenerator
+        );
+    }
+
+    /**
+     * @depends testIterateAddedAndRemovedDiff
+     */
+    public function testIterateComplexChangeDiff()
+    {
+        $diff = $this->loadDiff('diff_complex.diff');
+
+        $lineFeedGenerator = new ChunkLineFeedGenerator($diff[0]->chunks[0]);
+        $this->assertLinesFed(
+            array(42, 42, 43, 48, 48, 53, 53, 53),
+            $lineFeedGenerator
+        );
+
+        $lineFeedGenerator = new ChunkLineFeedGenerator($diff[0]->chunks[1]);
+        $this->assertLinesFed(
+            array(61, 62),
+            $lineFeedGenerator
+        );
+
+        $lineFeedGenerator = new ChunkLineFeedGenerator($diff[0]->chunks[2]);
+        $this->assertLinesFed(
+            array(95, 95),
+            $lineFeedGenerator
+        );
+
+        $lineFeedGenerator = new ChunkLineFeedGenerator($diff[0]->chunks[3]);
+        $this->assertLinesFed(
+            array(103, 103, 106, 106, 109, 109, 111, 111, 111, 111, 111, 111, 111, 111, 111, 111, 111, 111, 111, 111, 113, 113, 113, 113, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 125, 125, 125, 125),
+            $lineFeedGenerator
+        );
+    }
+
+    private function assertLinesFed($expectedLines, ChunkLineFeedGenerator $lineFeedGenerator)
+    {
+        $fedLines = array();
+        foreach ($lineFeedGenerator->feedLines() as $lineOffset => $lineNumber) {
+            $fedLines[$lineOffset] = $lineNumber;
+        }
+
+        $this->assertEquals(
+            $expectedLines,
+            $fedLines
+        );
+    }
+
+    private function loadDiff($fileName)
+    {
+        $parser = new Diff\Unified();
+        $diff = $parser->parseString(file_get_contents(__DIR__ . '/_fixtures/' . $fileName));
+        return $diff;
+    }
+}
