1d471d76f99c1d299c6a32b0cac0089eac122693
Tobias Schlitt <toby@php.net>
2013-09-28 15:00:22 +0200

    Implemented: Interface for observing the ChangeFeed.

---
+++ src/main/Qafoo/ChangeTrack/Analyzer/ChangeFeedObserver.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Qafoo\ChangeTrack\Analyzer;
+
+interface ChangeFeedObserver
+{
+    /**
+     * The ChangeFeed was initialized with $revisions
+     *
+     * @param array(string) $revisions
+     */
+    public function notifyInitialized(array $revisions);
+
+    /**
+     * The ChangeFeed is no processing $revision number $number.
+     *
+     * @param int $number
+     * @param string $revision
+     */
+    public function notifyProcessingRevision($number, $revision);
+
+    /**
+     * The ChangeFeed finished processing
+     */
+    public function notifyFinished();
+}
