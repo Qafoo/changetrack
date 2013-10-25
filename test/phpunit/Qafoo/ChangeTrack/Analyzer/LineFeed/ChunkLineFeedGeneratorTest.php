075c23ded9b777bb36f0a445b0ed8db636a2d4af
Tobias Schlitt <toby@php.net>
2013-10-25 21:45:45 +0200

    Fixed: ChunkLineFeedGeneratorTest after recent refactorings.

--- test/phpunit/Qafoo/ChangeTrack/Analyzer/LineFeed/ChunkLineFeedGeneratorTest.php
+++ test/phpunit/Qafoo/ChangeTrack/Analyzer/LineFeed/ChunkLineFeedGeneratorTest.php
@@ -3,6 +3,8 @@
 namespace Qafoo\ChangeTrack\Analyzer\LineFeed;
 
 use Qafoo\ChangeTrack\Analyzer\ChangeSet\Diff\LineChangeFeed\ChunkLineFeedGenerator;
+use Qafoo\ChangeTrack\Analyzer\Change\LineAddedChange;
+use Qafoo\ChangeTrack\Analyzer\Change\LineRemovedChange;
 use Arbit\VCSWrapper\Diff;
 
 /**
@@ -17,8 +19,11 @@ class ChunkLineFeedGeneratorTest extends \PHPUnit_Framework_TestCase
 
         $lineFeedGenerator = new ChunkLineFeedGenerator($diff[0]->chunks[0]);
 
-        $this->assertLinesFed(
-            array(21, 22),
+        $this->assertChangesFed(
+            array(
+                new LineAddedChange(21),
+                new LineAddedChange(22),
+            ),
             $lineFeedGenerator
         );
     }
@@ -29,8 +34,11 @@ class ChunkLineFeedGeneratorTest extends \PHPUnit_Framework_TestCase
 
         $lineFeedGenerator = new ChunkLineFeedGenerator($diff[0]->chunks[0]);
 
-        $this->assertLinesFed(
-            array(22, 22),
+        $this->assertChangesFed(
+            array(
+                new LineRemovedChange(22),
+                new LineRemovedChange(23),
+            ),
             $lineFeedGenerator
         );
     }
@@ -45,8 +53,11 @@ class ChunkLineFeedGeneratorTest extends \PHPUnit_Framework_TestCase
 
         $lineFeedGenerator = new ChunkLineFeedGenerator($diff[0]->chunks[0]);
 
-        $this->assertLinesFed(
-            array(22, 22),
+        $this->assertChangesFed(
+            array(
+                new LineRemovedChange(22),
+                new LineAddedChange(22),
+            ),
             $lineFeedGenerator
         );
     }
@@ -59,40 +70,95 @@ class ChunkLineFeedGeneratorTest extends \PHPUnit_Framework_TestCase
         $diff = $this->loadDiff('diff_complex.diff');
 
         $lineFeedGenerator = new ChunkLineFeedGenerator($diff[0]->chunks[0]);
-        $this->assertLinesFed(
-            array(42, 42, 43, 48, 48, 53, 53, 53),
+        $this->assertChangesFed(
+            array(
+                new LineRemovedChange(42),
+                new LineAddedChange(42),
+                new LineAddedChange(43),
+                new LineRemovedChange(47),
+                new LineAddedChange(48),
+                new LineRemovedChange(52),
+                new LineRemovedChange(53),
+                new LineAddedChange(53),
+            ),
             $lineFeedGenerator
         );
 
         $lineFeedGenerator = new ChunkLineFeedGenerator($diff[0]->chunks[1]);
-        $this->assertLinesFed(
-            array(61, 62),
+        $this->assertChangesFed(
+            array(
+                new LineAddedChange(61),
+                new LineAddedChange(62),
+            ),
             $lineFeedGenerator
         );
 
         $lineFeedGenerator = new ChunkLineFeedGenerator($diff[0]->chunks[2]);
-        $this->assertLinesFed(
-            array(95, 95),
+        $this->assertChangesFed(
+            array(
+                new LineRemovedChange(93),
+                new LineAddedChange(95)
+            ),
             $lineFeedGenerator
         );
 
         $lineFeedGenerator = new ChunkLineFeedGenerator($diff[0]->chunks[3]);
-        $this->assertLinesFed(
-            array(103, 103, 106, 106, 109, 109, 111, 111, 111, 111, 111, 111, 111, 111, 111, 111, 111, 111, 111, 111, 113, 113, 113, 113, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 125, 125, 125, 125),
+        $this->assertChangesFed(
+            array(
+                new LineRemovedChange(101),
+                new LineAddedChange(103),
+                new LineRemovedChange(104),
+                new LineAddedChange(106),
+                new LineRemovedChange(107),
+                new LineAddedChange(109),
+                new LineRemovedChange(109),
+                new LineRemovedChange(110),
+                new LineRemovedChange(111),
+                new LineRemovedChange(112),
+                new LineRemovedChange(113),
+                new LineRemovedChange(114),
+                new LineRemovedChange(115),
+                new LineRemovedChange(116),
+                new LineRemovedChange(117),
+                new LineRemovedChange(118),
+                new LineRemovedChange(119),
+                new LineRemovedChange(120),
+                new LineRemovedChange(121),
+                new LineAddedChange(111),
+                new LineRemovedChange(123),
+                new LineRemovedChange(124),
+                new LineRemovedChange(125),
+                new LineRemovedChange(126),
+                new LineAddedChange(113),
+                new LineAddedChange(114),
+                new LineAddedChange(115),
+                new LineAddedChange(116),
+                new LineAddedChange(117),
+                new LineAddedChange(118),
+                new LineAddedChange(119),
+                new LineAddedChange(120),
+                new LineAddedChange(121),
+                new LineAddedChange(122),
+                new LineAddedChange(123),
+                new LineRemovedChange(128),
+                new LineRemovedChange(129),
+                new LineRemovedChange(130),
+                new LineAddedChange(125),
+            ),
             $lineFeedGenerator
         );
     }
 
-    private function assertLinesFed($expectedLines, ChunkLineFeedGenerator $lineFeedGenerator)
+    private function assertChangesFed($expectedChanges, ChunkLineFeedGenerator $lineFeedGenerator)
     {
-        $fedLines = array();
+        $fedChanges = array();
         foreach ($lineFeedGenerator as $change) {
-            $fedLines[] = $change->affectedLine;
+            $fedChanges[] = $change;
         }
 
         $this->assertEquals(
-            $expectedLines,
-            $fedLines
+            $expectedChanges,
+            $fedChanges
         );
     }
 
