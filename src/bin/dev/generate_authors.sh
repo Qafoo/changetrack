81c7701c3b564a7e0392640516ef4add32d6f16d
Tobias Schlitt <toby@php.net>
2013-12-11 21:22:35 +0100

    Added: Explaination of AUTHORS file.

--- src/bin/dev/generate_authors.sh
+++ src/bin/dev/generate_authors.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+echo "Contributors sorted by number of contributions (i.e. Git commits):"
+echo ""
 git log --pretty="format:%an (%ae)" | grep -v 'waffle\.io' | sort | uniq -c | sort -n -r | sed 's/^\s*[0-9]\+ //g'
