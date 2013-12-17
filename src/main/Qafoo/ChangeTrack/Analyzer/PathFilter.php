8266f15a1b4ad422115a23df03e3227526a6eab5
Tobias Schlitt <toby@php.net>
2013-12-17 17:42:10 +0100

    Updated: Docs for PathFilter.

--- src/main/Qafoo/ChangeTrack/Analyzer/PathFilter.php
+++ src/main/Qafoo/ChangeTrack/Analyzer/PathFilter.php
@@ -4,15 +4,34 @@ namespace Qafoo\ChangeTrack\Analyzer;
 
 class PathFilter
 {
+    /**
+     * @see fnmatch()
+     * @var string[]
+     */
     private $paths;
+
+    /**
+     * @see fnmatch()
+     * @var string[]
+     */
     private $excludedPaths;
 
+    /**
+     * @param string[] $paths
+     * @param string[] $excludedPaths
+     */
     public function __construct($paths, $excludedPaths)
     {
         $this->paths = $paths;
         $this->excludedPaths = $excludedPaths;
     }
 
+    /**
+     * Returns true if $path should not be analyzed
+     *
+     * @param string $path
+     * @return bool
+     */
     public function isFiltered($path)
     {
         if ( ! empty($this->paths) && ! $this->matches($path, $this->paths)) {
@@ -26,6 +45,12 @@ class PathFilter
         return false;
     }
 
+    /**
+     * Returns true if $path matches at least on of $patterns
+     *
+     * @param string $path
+     * @param string[] $patterns {@link fnmatch()}
+     */
     private function matches($path, array $patterns)
     {
         foreach ($patterns as $pattern) {
