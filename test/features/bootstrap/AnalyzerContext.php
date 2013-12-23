d9c2ea9650903b883e67559703d5278f873eb53c
Tobias Schlitt <toby@php.net>
2013-12-23 09:15:00 +0100

    Refactored: Analyzer related Behat sentences into dedicated context.

---
+++ test/features/bootstrap/AnalyzerContext.php
@@ -0,0 +1,189 @@
+<?php
+
+use Behat\Behat\Context\BehatContext,
+    Behat\Behat\Exception\PendingException;
+use Behat\Gherkin\Node\TableNode;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+use Qafoo\ChangeTrack\RepositoryFactory;
+use Qafoo\ChangeTrack\Analyzer\RevisionBoundaries;
+use Qafoo\ChangeTrack\Analyzer\PathFilter;
+
+class AnalyzerContext extends BehatContext
+{
+    /**
+     * @var \Symfony\Component\DependencyInjection\ContainerInterface
+     */
+    private $container;
+
+    /**
+     * @var \Qafoo\ChangeTrack\Analyzer
+     */
+    private $analyzer;
+
+    /**
+     * @var string
+     */
+    private $repositoryUrl;
+
+    /**
+     * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * @return \Qafoo\ChangeTrack\Analyzer\Result
+     * @throws \RuntimeException if no changes were analyzes
+     */
+    public function getAnalyzedChanges()
+    {
+        if ($this->analyzedChanges === null) {
+            throw new \RuntimeException('No changes analyzed, yet.');
+        }
+        return $this->analyzedChanges;
+    }
+
+    /**
+     * @Given /^I have the repository$/
+     */
+    public function iHaveTheRepository()
+    {
+        $repositoryFactory = new RepositoryFactory();
+        $this->repositoryUrl = $repositoryFactory->getRepositoryUrl();
+    }
+
+    /**
+     * @When /^I analyze the changes$/
+     */
+    public function iAnalyzeTheChanges()
+    {
+        $this->iAnalyzeTheChangesFromTo(null, null);
+    }
+
+    /**
+     * @When /^I analyze the changes of paths "([^"]*)"$/
+     */
+    public function iAnalyzeTheChangesOfPaths($pathsStr)
+    {
+        $paths = preg_split('/\s*,\s*/', $pathsStr);
+
+        $this->iAnalyzeTheChangesFromTo(null, null, $paths);
+    }
+
+    /**
+     * @When /^I analyze the changes from "([^"]*)" to "([^"]*)"$/
+     */
+    public function iAnalyzeTheChangesFromTo($startRevision, $endRevision, array $paths = array())
+    {
+        $analyzer = $this->container->get('Qafoo.ChangeTrack.Analyzer');
+
+        $this->analyzedChanges = $analyzer->analyze(
+            $this->getRepositoryUrl(),
+            new RevisionBoundaries($startRevision, $endRevision),
+            new PathFilter($paths)
+        );
+    }
+
+    /**
+     * @Then /^there are no stats for revision "([^"]*)"$/
+     */
+    public function thereAreNoStatsForRevision($revision)
+    {
+        if (isset($this->analyzedChanges->revisionChanges[$revision])) {
+            throw new \RuntimeException(
+                sprintf(
+                    'Unexpected analysis result for revision "%s"',
+                    $revision
+                )
+            );
+        }
+    }
+
+    /**
+     * @Then /^there are the following stats in revision "([^"]*)"$/
+     */
+    public function thereAreTheFollowingStatsInRevision($revision, TableNode $table)
+    {
+        foreach ($table->getHash() as $rows) {
+            $package = $rows['Package'];
+            $class = $rows['Class'];
+            $method = $rows['Method'];
+            $added = $rows['Added'];
+            $removed = $rows['Removed'];
+
+            if (!isset($this->analyzedChanges->revisionChanges[$revision])) {
+                throw new \RuntimeException(
+                    sprintf(
+                        'Revision %s not found in stats.',
+                        $revision
+                    )
+                );
+            }
+            if (!isset($this->analyzedChanges->revisionChanges[$revision]->packageChanges[$package])) {
+                throw new \RuntimeException(
+                    sprintf(
+                        'Package %s not found in stats for revision %s.',
+                        $package,
+                        $revision
+                    )
+                );
+            }
+            if (!isset($this->analyzedChanges->revisionChanges[$revision]->packageChanges[$package]->classChanges[$class])) {
+                throw new \RuntimeException(
+                    sprintf(
+                        'Class %s from package %s not found in stats for revision %s.',
+                        $class,
+                        $package,
+                        $revision
+                    )
+                );
+            }
+            if (!isset($this->analyzedChanges->revisionChanges[$revision]->packageChanges[$package]->classChanges[$class]->methodChanges[$method])) {
+                throw new \RuntimeException(
+                    sprintf(
+                        'Method %s\%s::%s() not found in stats for revision %s.',
+                        $package,
+                        $class,
+                        $method,
+                        $revision
+                    )
+                );
+            }
+            if ($this->analyzedChanges->revisionChanges[$revision]->packageChanges[$package]->classChanges[$class]->methodChanges[$method]->numLinesAdded != $added) {
+                throw new \RuntimeException(
+                    sprintf(
+                        'Added stats for %s\%s::%s() incorrect for revision %s. Expected: %s. Actual: %s',
+                        $package,
+                        $class,
+                        $method,
+                        $revision,
+                        $added,
+                        $this->analyzedChanges[$revision][$package][$class][$method]->numLinesAdded
+                    )
+                );
+            }
+            if ($this->analyzedChanges->revisionChanges[$revision]->packageChanges[$package]->classChanges[$class]->methodChanges[$method]->numLinesRemoved != $removed) {
+                throw new \RuntimeException(
+                    sprintf(
+                        'Removed stats for %s\%s::%s() incorrect for revision %s. Expected: %s. Actual: %s',
+                        $package,
+                        $class,
+                        $method,
+                        $revision,
+                        $added,
+                        $this->analyzedChanges[$revision][$package][$class][$method]->numLinesRemoved
+                    )
+                );
+            }
+        }
+    }
+
+    private function getRepositoryUrl()
+    {
+        return $this->repositoryUrl;
+    }
+}
--- test/features/bootstrap/FeatureContext.php
+++ test/features/bootstrap/FeatureContext.php
@@ -6,8 +6,6 @@ use Behat\Gherkin\Node\TableNode;
 
 use Qafoo\ChangeTrack\Bootstrap;
 use Qafoo\ChangeTrack\RepositoryFactory;
-use Qafoo\ChangeTrack\Analyzer\RevisionBoundaries;
-use Qafoo\ChangeTrack\Analyzer\PathFilter;
 
 require __DIR__ . '/../../../vendor/autoload.php';
 
@@ -24,11 +22,6 @@ require __DIR__ . '/../../../vendor/autoload.php';
 class FeatureContext extends BehatContext
 {
     /**
-     * @var \Qafoo\ChangeTrack\Analyzer
-     */
-    private $analyzer;
-
-    /**
      * @var \Qafoo\ChangeTrack\Calculator\Stats
      */
     private $calculatedStats;
@@ -37,11 +30,6 @@ class FeatureContext extends BehatContext
      * @var \Symfony\Component\DependencyInjection\ContainerInterface
      */
     private $container;
-
-    /**
-     * @var string
-     */
-    private $repositoryUrl;
 
     /**
      * @var string
@@ -75,146 +63,16 @@ class FeatureContext extends BehatContext
         );
 
         $this->container->compile();
+
+        $this->useContext('analyzer', new AnalyzerContext($this->container));
     }
 
     /**
-     * @Given /^I have the repository$/
+     * @return \Qafoo\ChangeTrack\Analyzer\Result
      */
-    public function iHaveTheRepository()
+    public function getAnalyzedChanges()
     {
-        $repositoryFactory = new RepositoryFactory();
-        $this->repositoryUrl = $repositoryFactory->getRepositoryUrl();
-    }
-
-    /**
-     * @When /^I analyze the changes$/
-     */
-    public function iAnalyzeTheChanges()
-    {
-        $this->iAnalyzeTheChangesFromTo(null, null);
-    }
-
-    /**
-     * @When /^I analyze the changes of paths "([^"]*)"$/
-     */
-    public function iAnalyzeTheChangesOfPaths($pathsStr)
-    {
-        $paths = preg_split('/\s*,\s*/', $pathsStr);
-
-        $this->iAnalyzeTheChangesFromTo(null, null, $paths);
-    }
-
-    /**
-     * @When /^I analyze the changes from "([^"]*)" to "([^"]*)"$/
-     */
-    public function iAnalyzeTheChangesFromTo($startRevision, $endRevision, array $paths = array())
-    {
-        $analyzer = $this->container->get('Qafoo.ChangeTrack.Analyzer');
-
-        $this->analyzedChanges = $analyzer->analyze(
-            $this->getRepositoryUrl(),
-            new RevisionBoundaries($startRevision, $endRevision),
-            new PathFilter($paths)
-        );
-    }
-
-    /**
-     * @Then /^there are no stats for revision "([^"]*)"$/
-     */
-    public function thereAreNoStatsForRevision($revision)
-    {
-        if (isset($this->analyzedChanges->revisionChanges[$revision])) {
-            throw new \RuntimeException(
-                sprintf(
-                    'Unexpected analysis result for revision "%s"',
-                    $revision
-                )
-            );
-        }
-    }
-
-    private function getRepositoryUrl()
-    {
-        return $this->repositoryUrl;
-    }
-
-    /**
-     * @Then /^there are the following stats in revision "([^"]*)"$/
-     */
-    public function thereAreTheFollowingStatsInRevision($revision, TableNode $table)
-    {
-        foreach ($table->getHash() as $rows) {
-            $package = $rows['Package'];
-            $class = $rows['Class'];
-            $method = $rows['Method'];
-            $added = $rows['Added'];
-            $removed = $rows['Removed'];
-
-            if (!isset($this->analyzedChanges->revisionChanges[$revision])) {
-                throw new \RuntimeException(
-                    sprintf(
-                        'Revision %s not found in stats.',
-                        $revision
-                    )
-                );
-            }
-            if (!isset($this->analyzedChanges->revisionChanges[$revision]->packageChanges[$package])) {
-                throw new \RuntimeException(
-                    sprintf(
-                        'Package %s not found in stats for revision %s.',
-                        $package,
-                        $revision
-                    )
-                );
-            }
-            if (!isset($this->analyzedChanges->revisionChanges[$revision]->packageChanges[$package]->classChanges[$class])) {
-                throw new \RuntimeException(
-                    sprintf(
-                        'Class %s from package %s not found in stats for revision %s.',
-                        $class,
-                        $package,
-                        $revision
-                    )
-                );
-            }
-            if (!isset($this->analyzedChanges->revisionChanges[$revision]->packageChanges[$package]->classChanges[$class]->methodChanges[$method])) {
-                throw new \RuntimeException(
-                    sprintf(
-                        'Method %s\%s::%s() not found in stats for revision %s.',
-                        $package,
-                        $class,
-                        $method,
-                        $revision
-                    )
-                );
-            }
-            if ($this->analyzedChanges->revisionChanges[$revision]->packageChanges[$package]->classChanges[$class]->methodChanges[$method]->numLinesAdded != $added) {
-                throw new \RuntimeException(
-                    sprintf(
-                        'Added stats for %s\%s::%s() incorrect for revision %s. Expected: %s. Actual: %s',
-                        $package,
-                        $class,
-                        $method,
-                        $revision,
-                        $added,
-                        $this->analyzedChanges[$revision][$package][$class][$method]->numLinesAdded
-                    )
-                );
-            }
-            if ($this->analyzedChanges->revisionChanges[$revision]->packageChanges[$package]->classChanges[$class]->methodChanges[$method]->numLinesRemoved != $removed) {
-                throw new \RuntimeException(
-                    sprintf(
-                        'Removed stats for %s\%s::%s() incorrect for revision %s. Expected: %s. Actual: %s',
-                        $package,
-                        $class,
-                        $method,
-                        $revision,
-                        $added,
-                        $this->analyzedChanges[$revision][$package][$class][$method]->numLinesRemoved
-                    )
-                );
-            }
-        }
+        return $this->getMainContext()->getSubContext('analyzer')->getAnalyzedChanges();
     }
 
     /**
@@ -223,7 +81,7 @@ class FeatureContext extends BehatContext
     public function iCalculateTheStats()
     {
         $calculator = $this->container->get('Qafoo.ChangeTrack.Calculator');
-        $this->calculatedStats = $calculator->calculateStats($this->analyzedChanges);
+        $this->calculatedStats = $calculator->calculateStats($this->getAnalyzedChanges());
     }
 
     /**
