b1f00f9fe3268cb27d038ccef66bcce2b66e2242
Tobias Schlitt <toby@php.net>
2013-12-26 12:20:55 +0100

    Implemented: FrequentItemSet class. Refs. #29

---
+++ src/main/Qafoo/ChangeTrack/FISCalculator/FrequentItemSet.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Qafoo\ChangeTrack\FISCalculator;
+
+class FrequentItemSet
+{
+    /**
+     * @var \Qafoo\ChangeTrack\FISCalculator\Set
+     */
+    private $itemSet;
+
+    /**
+     * @var float
+     */
+    private $support;
+
+    /**
+     * @param \Qafoo\ChangeTrack\FISCalculator\Set $itemSet
+     * @param float $support
+     */
+    public function __construct(Set $itemSet, $support)
+    {
+        $this->itemSet = $itemSet;
+        $this->support = $support;
+    }
+
+    /**
+     * @return \Qafoo\ChangeTrack\FISCalculator\Set
+     */
+    public function getItemSet()
+    {
+        return $this->itemSet;
+    }
+
+    /**
+     * @return float
+     */
+    public function getSupport()
+    {
+        return $this->support;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return sprintf('%s (%01.2f)', (string) $this->itemSet, $this->support);
+    }
+}
