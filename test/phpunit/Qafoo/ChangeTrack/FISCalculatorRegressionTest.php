cd6ee6080fc8729438bcff5f482286c2a54eb4ea
Tobias Schlitt <toby@php.net>
2014-01-03 19:27:14 +0100

    Refactored: Test case to support multiple regresion tests. Refs. #31

--- test/phpunit/Qafoo/ChangeTrack/FISCalculatorRegressionTest.php
+++ test/phpunit/Qafoo/ChangeTrack/FISCalculatorRegressionTest.php
@@ -31,19 +31,40 @@ class FISCalculatorRegressionTest extends \PHPUnit_Framework_TestCase
         `$command frequent-item-sets -s '0.2' $inputFile > $resultFile`;
 
         $this->assertXmlStringEqualsXmlString(
+            $this->loadExpectedXml('method'),
+            file_get_contents($resultFile)
+        );
+    }
+
+    /*
+    public function testCalculatorRegressionWithClasses()
+    {
+        $command = $this->testTools->getApplicationPath('src/bin/track');
+        $inputFile = $this->testTools->getApplicationPath('test/phpunit/_fixtures/regression_analysis_daemon.xml');
+        $resultFile = $this->resultFile;
+
+        `$command frequent-item-sets -s '0.2 -i class' $inputFile > $resultFile`;
+
+        $this->assertXmlStringEqualsXmlString(
             $this->loadExpectedXml(),
             file_get_contents($resultFile)
         );
     }
+    */
 
     /**
      * @param string $repositoryUrl
      * @return string
      */
-    private function loadExpectedXml()
+    private function loadExpectedXml($suffix)
     {
         $referenceXml = file_get_contents(
-            $this->testTools->getApplicationPath('test/phpunit/_fixtures/regression_fiscalculate_daemon.xml')
+            $this->testTools->getApplicationPath(
+                sprintf(
+                    'test/phpunit/_fixtures/regression_fiscalculate_daemon_%s.xml',
+                    $suffix
+                )
+            )
         );
 
         return $referenceXml;
