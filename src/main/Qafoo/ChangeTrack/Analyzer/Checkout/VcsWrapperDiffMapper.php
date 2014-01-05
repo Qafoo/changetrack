3aef092e12b1b555ecd00debcd07f1ff548d904f
Tobias Schlitt <toby@php.net>
2014-01-05 22:00:20 +0100

    Implemented: Mapping from VCSWrapper to internal diff representation. Refs. #24

---
+++ src/main/Qafoo/ChangeTrack/Analyzer/Checkout/VcsWrapperDiffMapper.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Qafoo\ChangeTrack\Analyzer\Checkout;
+
+use Arbit\VCSWrapper\Diff\Collection as VcsDiffCollection;
+use Arbit\VCSWrapper\Diff\Chunk as VcsDiffChunk;
+use Arbit\VCSWrapper\Diff\Line as VcsDiffLine;
+
+use Qafoo\ChangeTrack\Analyzer\Diff\Diff;
+use Qafoo\ChangeTrack\Analyzer\Diff\Chunk;
+use Qafoo\ChangeTrack\Analyzer\Diff\Line;
+
+class VcsWrapperDiffMapper
+{
+    /**
+     * @var array
+     */
+    private $typeMap = array(
+        VcsDiffLine::ADDED => Line::ADDED,
+        VcsDiffLine::REMOVED => Line::REMOVED,
+        VcsDiffLine::UNCHANGED => Line::UNCHANGED,
+    );
+
+    /**
+     * Maps a VCSWrapper Diff to the ChangeTrack diff representation.
+     *
+     * @param \Arbit\VCSWrapper\Diff\Collection $vcsDiff
+     * @return \Qafoo\ChangeTrack\Analyzer\Diff\Diff
+     */
+    public function mapDiff(VcsDiffCollection $vcsDiff)
+    {
+        return new Diff(
+            $vcsDiff->from,
+            $vcsDiff->to,
+            $this->mapChunks($vcsDiff->chunks)
+        );
+    }
+
+    /**
+     * @param \Arbit\VCSWrapper\Diff\Chunk[] $vcsChunks
+     * @return \Qafoo\ChangeTrack\Analyzer\Diff\Chunk[]
+     */
+    private function mapChunks(array $vcsChunks)
+    {
+        $mappedChunks = array();
+        foreach ($vcsChunks as $vcsChunk) {
+            $mappedChunks[] = $this->mapChunk($vcsChunk);
+        }
+        return $mappedChunks;
+    }
+
+    /**
+     * @param \Arbit\VCSWrapper\Diff\Chunk $vcsChunk
+     * @return \Qafoo\ChangeTrack\Analyzer\Diff\Chunk
+     */
+    private function mapChunk(VcsDiffChunk $vcsChunk)
+    {
+        return new Chunk(
+            $vcsChunk->start,
+            $vcsChunk->startRange,
+            $vcsChunk->end,
+            $vcsChunk->endRange,
+            $this->mapLines($vcsChunk->lines)
+        );
+    }
+
+    /**
+     * @param \Arbit\VCSWrapper\Diff\Line[] $vcsLines
+     * @return \Qafoo\ChangeTrack\Analyzer\Diff\Line[]
+     */
+    private function mapLines(array $vcsLines)
+    {
+        $mappedLines = array();
+        foreach ($vcsLines as $vcsLine) {
+            $mappedLines[] = $this->mapLine($vcsLine);
+        }
+        return $mappedLines;
+    }
+
+    /**
+     * @param \Arbit\VCSWrapper\Diff\Line $vcsLine
+     * @return \Qafoo\ChangeTrack\Analyzer\Diff\Line
+     */
+    private function mapLine(VcsDiffLine $vcsLine)
+    {
+        return new Line(
+            $this->mapType($vcsLine->type),
+            $vcsLine->content
+        );
+    }
+
+    /**
+     * @param const $vcsType
+     * @return const
+     */
+    private function mapType($vcsType)
+    {
+        return $this->typeMap[$vcsType];
+    }
+}
---
+++ test/phpunit/Qafoo/ChangeTrack/Analyzer/Checkout/VcsWrapperDiffMapperTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Qafoo\ChangeTrack\Analyzer\Checkout;
+
+use Arbit\VCSWrapper\Diff\Collection as VcsDiffCollection;
+use Arbit\VCSWrapper\Diff\Chunk as VcsDiffChunk;
+use Arbit\VCSWrapper\Diff\Line as VcsDiffLine;
+
+use Qafoo\ChangeTrack\Analyzer\Diff\Diff;
+use Qafoo\ChangeTrack\Analyzer\Diff\Chunk;
+use Qafoo\ChangeTrack\Analyzer\Diff\Line;
+
+class VcsWrapperDiffMapperTest extends \PHPUnit_Framework_TestCase
+{
+    public function testMapDiff()
+    {
+        $input = new VcsDiffCollection(
+            'foo.txt',
+            'bar.txt',
+            array(
+                new VcsDiffChunk(
+                    1,
+                    11,
+                    2,
+                    22,
+                    array(
+                        new VcsDiffLine(VcsDiffLine::ADDED, 'content'),
+                    )
+                )
+            )
+        );
+
+        $expectedOutput = new Diff(
+            'foo.txt',
+            'bar.txt',
+            array(
+                new Chunk(
+                    1,
+                    11,
+                    2,
+                    22,
+                    array(
+                        new Line(Line::ADDED, 'content'),
+                    )
+                )
+            )
+        );
+
+        $mapper = new VcsWrapperDiffMapper();
+        $actualOutput = $mapper->mapDiff($input);
+
+        $this->assertEquals($expectedOutput, $actualOutput);
+    }
+}
